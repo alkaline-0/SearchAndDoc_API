4ed63561e736b23a88830c3982f972aa37a4e7de
Menna Tullah Magdy Taha <menna.tullah.magdy731@gmail.com>
2025-04-21 15:19:08 +0200

    UPD: added all collection creation tests

--- tests/db/test_solr_admin.py
+++ tests/db/test_solr_admin.py
@@ -1,7 +1,7 @@
 import pytest
 
 from db.solr_utils.solr_admin import SolrAdminClient
-from db.solr_utils.solr_exceptions import SolrConnectionError
+from db.solr_utils.solr_exceptions import SolrConnectionError, SolrValidationError
 from tests.db.mocks.mock_solr_config import MockSolrConfig
 
 
@@ -15,7 +15,45 @@ class TestSolrAdmin:
             )
             solr_admin_client.create_collection(collection_name="test_collection")
 
-            assert "401 Client Error: Unauthorized for url" in str(excinfo.value)
+        assert "401 Client Error: Unauthorized for url" in str(excinfo.value)
+
+    def test_successful_collection_creation(self):
+        """Test successful collection creation."""
+        cfg = MockSolrConfig()
+        solr_admin_client = SolrAdminClient(cfg=cfg)
+        collection_name = "test_collection"
+        collection_url = solr_admin_client.create_collection(
+            collection_name=collection_name
+        )
+
+        assert (
+            collection_url
+            == f"http://{cfg.SOLR_HOST}:{cfg.SOLR_PORT}/solr/{collection_name}"
+        )
+
+    def test_unsuccessful_collection_creation_empty_name(self):
+        """Test unsuccessful collection creation due to empty name."""
+        with pytest.raises(SolrValidationError) as excinfo:
+            cfg = MockSolrConfig()
+            solr_admin_client = SolrAdminClient(cfg=cfg)
+            solr_admin_client.create_collection(collection_name="")
+        assert "Collection name cannot be empty" in str(excinfo.value)
+
+    def test_successful_return_collection_url_for_creating_collection_with_existing_name(
+        self,
+    ):
+        """Test unsuccessful collection creation due to already existing collection."""
+        cfg = MockSolrConfig()
+        solr_admin_client = SolrAdminClient(cfg=cfg)
+        collection_name = "test_collection"
+        collection = solr_admin_client.create_collection(
+            collection_name=collection_name
+        )
+
+        collection_dup = solr_admin_client.create_collection(
+            collection_name=collection_name
+        )
+        assert collection == collection_dup
 
 
 # class TestSolrCore:
